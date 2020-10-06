fc7bd9bd22c29a1ebf97245adf9b91345e80406b
Miguel Munoz <memg92@gmail.com>
2020-10-06 18:13:55 -0300

    finished phase 4

--- public/events.js
+++ public/events.js
@@ -1,14 +1,17 @@
 const newPicBtn = document.getElementById("new-pic");
 const loader = document.querySelector(".loader");
-const errorField = document.querySelector(".error")
+const errorField = document.querySelector(".error");
 const upVote = document.querySelector("#upvote");
 const downVote = document.querySelector("#downvote");
+const score = document.querySelector(".score");
+const commentPost = document.querySelector(".comment-form");
 
 document.addEventListener("DOMContentLoaded", async (event) => {
   const res = await fetch("/kitten/image");
   const json = await res.json();
   console.log(json);
 
+  //this could potentially be refactored (lines 14-18)
   if (!res.ok) {
     errorField.innerHTML = json.message;
   } else {
@@ -22,16 +25,39 @@ newPicBtn.addEventListener("click", async (event) => {
   const json = await res.json();
   loader.innerHTML = "";
 
+  //this could potentially be refactored (lines 27-32)
   if (!res.ok) {
-    console.log("error");
+    errorField.innerHTML = json.message;
   } else {
     document.querySelector("img").src = json.src;
+    score.innerHTML = 0;
   }
 });
 
 upVote.addEventListener("click", async (event) => {
-  const res = await fetch("/kitten/upvote");
+  const res = await fetch("/kitten/upvote", {
+    method: "PATCH",
+  });
   const json = await res.json();
-  console.log(json)
 
-})
+  //this could potentially be refactored (lines 41-45)
+  if (!res.ok) {
+    errorField.innerHTML = json.message;
+  } else {
+    score.innerHTML = json.score;
+  }
+});
+
+downVote.addEventListener("click", async (event) => {
+  const res = await fetch("/kitten/downvote", {
+    method: "PATCH",
+  });
+  const json = await res.json();
+
+  //this could potentially be refactored (lines 57-61)
+  if (!res.ok) {
+    errorField.innerHTML = json.message;
+  } else {
+    score.innerHTML = json.score;
+  }
+});
