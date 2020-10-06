20d0b9c9a37ab3ddc3728f309900847e97fa0cdd
Miguel Munoz <memg92@gmail.com>
2020-10-06 17:20:54 -0300

    finished phase 2

--- public/events.js
+++ public/events.js
@@ -1,9 +1,29 @@
+const newPicBtn = document.getElementById("new-pic");
+const loader = document.querySelector(".loader");
 
 document.addEventListener("DOMContentLoaded", async (event) => {
+  const res = await fetch("/kitten/image");
+  const json = await res.json();
+  console.log(json);
 
-    const res = await fetch("/kitten/image");
-    const json = await res.json();
-    console.log(json)
+  if (!res.ok) {
+    console.log("error");
+  } else {
+    document.querySelector("img").src = json.src;
+  }
+});
 
+// document.addEventListener("")
 
-})
+newPicBtn.addEventListener("click", async (event) => {
+  loader.innerHTML = "Loading...";
+  const res = await fetch("/kitten/image");
+  const json = await res.json();
+  loader.innerHTML = "";
+
+  if (!res.ok) {
+    console.log("error");
+  } else {
+    document.querySelector("img").src = json.src;
+  }
+});
